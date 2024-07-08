2bac8b0fe29ad31ae008676240ec7e76566b20a0
Hanny <flayrahanny@hotmail.com.br>
2024-07-08 18:34:39 -0300

    adicionando classes Vacina, Exame e Remedio

---
+++ src/modelo/Exame.java
@@ -0,0 +1,65 @@
+package modelo;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+
+public class Exame implements Serializable{
+  private static int codExame=1;
+  private String nomeExame;
+  private String descricao;
+  private Date data;
+  private String hora;
+
+    public Exame (String nome, String descricao, Date data, String hora){
+      setNomeExame(nome);
+      setDescricao(descricao);
+      setData(data);
+      setHora(hora);
+      codExame++;
+    }
+
+  public int getCodExame(){
+    return codExame;
+  }
+
+  public void setCodExame(int codExame){
+    Exame.codExame = codExame;
+  }
+  
+  public String getNomeExame() {
+    return nomeExame;
+  }
+
+  public void setNomeExame(String nomeExame) {
+    this.nomeExame = nomeExame;
+  }
+  
+  public String getDescricao() {
+  return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public Date getData() {
+  return data;
+  }
+
+  public void setData(Date data) {
+    this.data = data;
+  }
+
+  public String getHora() {
+  return hora;
+  }
+
+  public void setHora(String hora) {
+    this.hora = hora;
+  }
+
+
+
+  
+}
