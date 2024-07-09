6c7c3d3886e80ebf83b4457c012017c7c1faf127
Hanny <flayrahanny@hotmail.com.br>
2024-07-09 18:12:55 -0300

    ProfissionalController

---
+++ src/controle/controle_back/ProfissionalController.java
@@ -0,0 +1,37 @@
+package controle.controle_back;
+
+import java.util.ArrayList;
+
+import data.DAO.IProfissionalDAO;
+import data.JBDC.ProfissionalDaoJdbc;
+import modelo.Profissional;
+
+public class ProfissionalController {
+    IProfissionalDAO ProfissionalDAO = new ProfissionalDaoJdbc();
+
+    public void cadastrarProfissional (String nome, long cpf, long telefone, String funcao, String email, String senha){
+        Profissional profissional = new Profissional(nome, cpf, telefone, funcao, email, senha);
+        ProfissionalDAO.createProfissional(profissional);
+    }
+    
+    public ArrayList<Profissional> listarProfissional(){
+        ArrayList<Profissional> profissionals = (ArrayList<Profissional>) ProfissionalDAO.getAllProfissional();
+        return profissionals;
+    }
+
+    public Profissional pesquisarProfissionals (Long cpf){
+      return ProfissionalDAO.readProfissional(cpf);
+    }
+
+    public void atualizarProfissional (String nome, long cpf, long telefone, String funcao, String email, String senha){
+      Profissional Profissional = new Profissional(nome, cpf, telefone, funcao, email, senha);
+      ProfissionalDAO.updateProfissional(Profissional);
+    }
+
+    public void deletarProfissional (String nome, long cpf, long telefone, String funcao, String email, String senha){
+      Profissional Profissional = new Profissional(nome, cpf, telefone, funcao, email, senha);
+      ProfissionalDAO.deleteProfissional(Profissional);
+    }
+
+
+}
