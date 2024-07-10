963563b92bdf8ae502aa36af97bdf01ca89cf4d9
Thiago Fernandes <thi19fernandes@gmail.com>
2024-07-10 18:55:31 -0300

    VACINA

--- src/modelo/Exame.java
+++ src/modelo/Exame.java
@@ -3,34 +3,27 @@ package modelo;
 import java.io.Serializable;
 import java.sql.Date;
 
-
-public class Exame implements Serializable{
+public class Exame implements Serializable {
   private int codExame;
   private String nomeExame;
   private String descricao;
-  private Date data;
-  private String hora;
 
-  
-
-    public Exame() {
+  public Exame() {
   }
 
-    public Exame (String nome, String descricao, Date data, String hora){
-      setNomeExame(nome);
-      setDescricao(descricao);
-      setData(data);
-      setHora(hora);
-    }
+  public Exame(String nome, String descricao) {
+    setNomeExame(nome);
+    setDescricao(descricao);
+  }
 
-  public int getCodExame(){
+  public int getCodExame() {
     return codExame;
   }
 
-  public void setCodExame(int codExame){
+  public void setCodExame(int codExame) {
     this.codExame = codExame;
   }
-  
+
   public String getNomeExame() {
     return nomeExame;
   }
@@ -38,32 +31,12 @@ public class Exame implements Serializable{
   public void setNomeExame(String nomeExame) {
     this.nomeExame = nomeExame;
   }
-  
+
   public String getDescricao() {
-  return descricao;
+    return descricao;
   }
 
   public void setDescricao(String descricao) {
     this.descricao = descricao;
   }
-
-  public Date getData() {
-  return data;
-  }
-
-  public void setData(Date data) {
-    this.data = data;
-  }
-
-  public String getHora() {
-  return hora;
-  }
-
-  public void setHora(String hora) {
-    this.hora = hora;
-  }
-
-
-
-  
 }
